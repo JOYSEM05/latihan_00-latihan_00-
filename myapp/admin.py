7462a8d17eedc6e1acdf8bf61bcadf353bb8a3f1
JOYSEM05 <joysembiring91@gmail.com>
2025-05-05 00:30:19 +0700

    Initial commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,10 @@
-from django.db import models
-
 from django.contrib import admin
-from mynewapp.models import StatusModel  # impor model, bukan definisi ulang
+from .models import Author, Book, Album, Song, Vehicle, Car, GeeksModel
 
-admin.site.register(StatusModel)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(Vehicle)
+admin.site.register(Car)
+admin.site.register(GeeksModel)
--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,47 @@
 from django.db import models
 
-class StatusModel(models.Model):
-    name = models.CharField(max_length=200)
+# Model Author
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+# Model Book
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+
+# Model Album
+class Album(models.Model):
+    name = models.CharField(max_length=100)
+
+# Model Song
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+# Model Vehicle
+class Vehicle(models.Model):
+    reg_no = models.IntegerField()
+
+    def __str__(self):
+        return str(self.reg_no)
+
+# Model Car
+class Car(models.Model):
+    vehicle = models.OneToOneField(Vehicle, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.vehicle)
+
+# Menambahkan GeeksModel
+class GeeksModel(models.Model):
+    name = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+
 
    

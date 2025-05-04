1c47c0d5178bafbdfd392e128341dd61b1fea080
JOYSEM05 <joysembiring91@gmail.com>
2025-05-04 10:03:23 +0700

    Initial commit with .gitignore

---
+++ my_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import StatusModels
+
+admin.site.register(StatusModels)
--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from django.contrib import admin
+from mynewapp.models import StatusModel  # impor model, bukan definisi ulang
+
+admin.site.register(StatusModel)
